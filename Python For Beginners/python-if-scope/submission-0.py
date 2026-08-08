d6167045661a2ee0fae096685e0d56f961ddd593
Rhyannon Rodriguez <40784317+rhyannonjoy@users.noreply.github.com>
2026-08-07 20:12:30 -0700

    Add: python-if-scope - submission-0

---
+++ Python For Beginners/python-if-scope/submission-0.py
@@ -0,0 +1,10 @@
+def pay_bill(balance: int, bill: int) -> int:
+    new_balance = balance - bill
+    if balance >= bill:
+        return new_balance
+    return balance
+
+# do not modify below this line
+print(pay_bill(100, 50))
+print(pay_bill(100, 100))
+print(pay_bill(100, 150))
